cb7663e7e0c1e426b65c7a6adcb030e4adce7a51
doup <asier@illarra.com>
2017-09-15 21:20:06 +0200

    Add origin/target and change colors

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,18 @@ struct Grid {
     grid: Vec<u8>,
 }
 
+struct GridPoint {
+    x: u8,
+    y: u8,
+}
+
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 3],
 }
 
-fn shape_from_grid(grid: &Grid, type_filter: u8) -> Vec<Vertex> {
+fn shape_from_grid(grid: &Grid, padding: f32, type_filter: u8) -> Vec<Vertex> {
     let mut shape   = vec![];
-    let padding     = 0.01;
     let cell_width  = 2.0 / grid.width as f32;
     let cell_height = 2.0 / grid.height as f32;
 
@@ -40,7 +44,25 @@ fn shape_from_grid(grid: &Grid, type_filter: u8) -> Vec<Vertex> {
     shape
 }
 
+fn shape_from_gridpoint(grid: &Grid, point: &GridPoint, width: f32, z: f32) -> Vec<Vertex> {
+    let cell_height = 2.0 / grid.height as f32;
+    let cell_width = 2.0 / grid.width as f32;
+    let x = -1.0 + (point.x as f32 * cell_width) + cell_width / 2.0;
+    let y =  1.0 - (point.y as f32 * cell_height) - cell_height / 2.0;
+
+    let shape = vec![
+        Vertex { position: [x - width, y + width, z] },
+        Vertex { position: [x + width, y + width, z] },
+        Vertex { position: [x - width, y - width, z] },
+        Vertex { position: [x + width, y - width, z] },
+    ];
+
+    shape
+}
+
 fn main() {
+    let mut origin = GridPoint { x: 0, y: 0 };
+    let mut target = GridPoint { x: 0, y: 0 };
     let grid1 = Grid {
         width: 9,
         height: 7,
@@ -72,11 +94,11 @@ fn main() {
         }
     "#;
 
-    let fragment_shader_src = r#"
+    let walkable_fragment_shader_src = r#"
         #version 140
         out vec4 color;
         void main() {
-            color = vec4(0.9, 0.9, 0.9, 1.0);
+            color = vec4(0.8, 0.94, 1.0, 1.0);
         }
     "#;
 
@@ -84,19 +106,45 @@ fn main() {
         #version 140
         out vec4 color;
         void main() {
-            color = vec4(0.7, 0.7, 0.7, 1.0);
+            color = vec4(0.07, 0.31, 0.41, 1.0);
         }
     "#;
 
-    let walkable_obj_shape = shape_from_grid(&grid1, 0);
+    let origin_fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(0, 0.68, 0.48, 1.0);
+        }
+    "#;
+
+    let target_fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(0.65, 0.01, 0.25, 1.0);
+        }
+    "#;
+
+    let walkable_obj_shape = shape_from_grid(&grid1, 0.01, 0);
     let walkable_obj_vertex_buffer = glium::VertexBuffer::new(&display, &walkable_obj_shape).unwrap();
     let walkable_obj_indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
-    let walkable_obj_program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let walkable_obj_program = glium::Program::from_source(&display, vertex_shader_src, walkable_fragment_shader_src, None).unwrap();
 
-    let wall_obj_shape = shape_from_grid(&grid1, 1);
+    let wall_obj_shape = shape_from_grid(&grid1, 0.0, 1);
     let wall_obj_vertex_buffer = glium::VertexBuffer::new(&display, &wall_obj_shape).unwrap();
     let wall_obj_indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
     let wall_obj_program = glium::Program::from_source(&display, vertex_shader_src, wall_fragment_shader_src, None).unwrap();
+
+    let origin_obj_shape = shape_from_gridpoint(&grid1, &origin, 0.07, 0.1);
+    let origin_obj_vertex_buffer = glium::VertexBuffer::new(&display, &origin_obj_shape).unwrap();
+    let origin_obj_indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
+    let origin_obj_program = glium::Program::from_source(&display, vertex_shader_src, origin_fragment_shader_src, None).unwrap();
+
+    let target_obj_shape = shape_from_gridpoint(&grid1, &target, 0.03, 0.2);
+    let target_obj_vertex_buffer = glium::VertexBuffer::new(&display, &target_obj_shape).unwrap();
+    let target_obj_indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
+    let target_obj_program = glium::Program::from_source(&display, vertex_shader_src, target_fragment_shader_src, None).unwrap();
 
     let mut closed = false;
     while !closed {
@@ -115,6 +163,22 @@ fn main() {
             &wall_obj_vertex_buffer,
             &wall_obj_indices,
             &wall_obj_program,
+            &glium::uniforms::EmptyUniforms,
+            &Default::default()
+        ).unwrap();
+
+        target.draw(
+            &origin_obj_vertex_buffer,
+            &origin_obj_indices,
+            &origin_obj_program,
+            &glium::uniforms::EmptyUniforms,
+            &Default::default()
+        ).unwrap();
+
+        target.draw(
+            &target_obj_vertex_buffer,
+            &target_obj_indices,
+            &target_obj_program,
             &glium::uniforms::EmptyUniforms,
             &Default::default()
         ).unwrap();
